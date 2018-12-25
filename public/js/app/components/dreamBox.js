77bff658e89ccd0b7e784adbd4ab29c6a3c8e243
Michaelpalacce <stefantigro@gmail.com>
2018-12-25 19:04:46 +0200

    [master] Added validation to the dream box as well as a submit button

--- public/js/app/components/dreamBox.js
+++ public/js/app/components/dreamBox.js
@@ -14,6 +14,32 @@ define( 'DreamBox', ['Component'], function ( Component )
 		{
 			super( props );
 			this.detach();
+			this.maxLength	= 96;
+			this.dreamInput	= this.element.find( '#dream-input' );
+		}
+
+		attachEvents()
+		{
+			super.attachEvents();
+			this.element.find( '#dream-input' ).on( 'input', ( event )=>{
+				let text			= this.dreamInput.val();
+				let lengthOfText	= text.length;
+
+				if ( lengthOfText > this.maxLength )
+				{
+					text	= text.substr( 0, this.maxLength );
+				}
+
+				this.dreamInput.val( text );
+				this.setState( {
+					inputValue	: text,
+					inputLength	: text.length
+				} );
+			});
+
+			this.addOnStateChanged( 'inputLength', ( value )=>{
+				$( '#dream-box-message-length' ).text( value );
+			} );
 		}
 
 		/**
@@ -23,7 +49,11 @@ define( 'DreamBox', ['Component'], function ( Component )
 		 */
 		_render()
 		{
-			return '<div class="dream-box"> <textarea placeholder="\nPenny for a dream?" class="dream-input" autofocus></textarea> </div>';
+			return '<div class="dream-box">' +
+					'<textarea placeholder="\nPenny for a dream?" class="dream-input" autofocus id="dream-input"></textarea>' +
+					'<div class="counter" style="color: #000;"><span id="dream-box-message-length">0</span><span>/96</span></div>' +
+					'<button class="dream-submit">Share</button>' +
+				'</div>';
 		}
 	}
 
